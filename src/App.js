0c69b6061afe76c4746aece466317f19de4d757a
divakar10d <divakar10d@gmail.com>
2022-02-07 10:38:36 +0530

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import DisplayCards from './components/DisplayCards';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import DisplayCards from "./components/DisplayCards";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./components/style.css";
-import axios from 'axios';
-import Details from './components/Details';
-import styled from 'styled-components';
+import axios from "axios";
+import Details from "./components/Details";
+import styled from "styled-components";
 // import data from './data'
 // import List from './List';
 const StyledOuterDiv = styled.div`
@@ -12,39 +12,36 @@ const StyledOuterDiv = styled.div`
 `;
 
 const App = () => {
-
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   const [characters, setCharacters] = useState([]);
-  
+
   useEffect(() => {
     getCharactersInfo();
-  }, [])
+  }, []);
 
-  const getCharactersInfo = async() => {
-    const res = await axios.get("https://rickandmortyapi.com/api/character")
+  const getCharactersInfo = async () => {
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
     const json = res.data.results;
     setCharacters(json);
-  }
+  };
 
-  return(
-    
+  return (
     <div>
       <StyledOuterDiv>
-      <Router>
-        <Switch>
-          <Route path={`/details/:id`}>
-            <Details />
-          </Route>
-          <Route path="/">
-            <DisplayCards characters={characters} />
-          </Route>
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route path={`/details/:id`}>
+              <Details />
+            </Route>
+            <Route path="/">
+              <DisplayCards characters={characters} />
+            </Route>
+          </Switch>
+        </Router>
       </StyledOuterDiv>
-      </div>
-    
-  )
-}
+    </div>
+  );
+};
 
 export default App;
